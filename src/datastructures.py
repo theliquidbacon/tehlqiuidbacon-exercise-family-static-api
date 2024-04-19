3c5f9ad42b7418787af336077dec1646332b471a
theliquidbacon <146387957+theliquidbacon@users.noreply.github.com>
2024-04-19 09:18:53 +0000

    solvedsorry

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,47 +1,56 @@
-
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self._members = [
+            {
+                "id": 3443,
+                "first_name": "Tommy",
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": 4446,
+                "first_name": "Jane",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": 5522,
+                "first_name": "Jimmy",
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
-        self._next_id = 1
-        self._members = []
-
-    # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
-        generated_id = self._next_id
-        self._next_id += 1
-        return generated_id
+        return randint(0, 99999999)
 
-    def add_member(self, first_name, age, lucky_numbers):
-        new_member = {
-            "id": self._generateId(),
-            "first_name": first_name,
-            "age": age,
-            "lucky_numbers": lucky_numbers
-        }
-        self._members.append(new_member)
+    def add_member(self, member):
+        if "id" not in member:
+            member["id"] = self._generateId()
+        self._members.append(member)
 
-    def delete_member(self, id):
-        for i, member in enumerate(self._members):
-            if member["id"] == id:
-             del self._members[i]
-            return member
-
-    def get_member(self, id):
+    def delete_member(self, member_id):
         for member in self._members:
-            if member["id"] == id:
-                return member
-        return None
+            if member["id"] == member_id:
+                self._members.remove(member)
+                return
+        raise ValueError("Member not found")
 
-    # this method is done, it returns a list with all the family members
+    def update_member(self, member_id, member):
+        for i, m in enumerate(self._members):
+            if m["id"] == member_id:
+                member["id"] = member_id
+                self._members[i] = member
+                return
+        raise ValueError("Member not found")
+
+    def get_member(self, member_id):
+        for member in self._members:
+            if member["id"] == member_id:
+                return member
+        raise ValueError("Member not found")
     def get_all_members(self):
         return self._members
